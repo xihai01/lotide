6fc49abb12577fb77044d4e9fe8ac1091cd97faa
Xihai Luo <xihai.luo01@gmail.com>
2021-10-27 19:29:44 +0000

    added countOnly.js which counts specific strings appearing in an array and returns an object listing those counts of occurences

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+//function implementation
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`🟩🟩🟩 Assertion Passed: ${actual} === ${expected}`) : console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//allItems: an array of strings that we need to look through
+//itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item]++;
+      } else {
+        //create new key-value pair in results
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+//test cases
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
