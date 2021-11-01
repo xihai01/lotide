573a052a90527167d90171dfaabcbebbb25a6e8f
Xihai Luo <xihai.luo01@gmail.com>
2021-11-01 01:48:08 +0000

    refactored eqArrays.js with recursion to handle nested arrays

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Lotide is a copy of [Lodash](https://github.com/lodash/lodash) which is a modern
 * assertEqual => (actual, expected) => true/false string comparisons
 * head => (array) => returns first element in array
 * tail => (array) => slices off first element and returns rest of array
-* eqArrays => (arr1, arr2) => returns a boolean indicating whether arr1 is equal to arr2 in value, type and length
+* eqArrays => (arr1, arr2) => returns a boolean indicating whether arr1 is equal to arr2 in value, type and length. Can supported nested arrays.
 * assertArraysEqual => (arr1, arr2) => prints an assertion message to console from comparing two arrays
 * without => (arr1, arr2) => arr2 is an array of values to be removed from arr1. without() returns a new array of arr1 with values of arr2 removed
 * flatten => (array) => outputs a single array of elements from a multi-dimensional array. **This function is still under construction. In the future, refactor so it uses recursion to handle infinite nested arrays**
--- eqArrays.js
+++ eqArrays.js
@@ -5,12 +5,19 @@ const assertEqual = function(actual, expected) {
 
 //function returns true if both arrays are equal in length, value and type
 const eqArrays = function(arr1, arr2) {
+  //check both arrays are the same type
   //arr1 is not equal to arr2 if both arrays have different lengths
   if (arr1.length !== arr2.length) return false;
   //loop through each element of both arrays
   for (let i = 0; i < arr1.length; i++) {
-    //return false immediately once different elements are detected
-    if (arr1[i] !== arr2[i]) return false;
+    //check if element is an array
+    if (Array.isArray(arr1[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false;
+    }
   }
   //both arrays are equal, return true
   return true;
@@ -26,3 +33,10 @@ console.log(assertEqual(eqArrays([1, '2', 3], [1, '2', 3]), true));
 console.log(assertEqual(eqArrays([1, 2, 3], []), false));
 console.log(assertEqual(eqArrays([], [1, 2, 3]), false));
 console.log(assertEqual(eqArrays([], []), true));
+
+//nested arrays
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)); //true
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)); //false
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)); //false
+console.log(assertEqual(eqArrays([[2, [3, 4]], [4, 5], 'hi'], [[2, [3, 4]], [4, 5], 'hi']), true)); //true
+console.log(assertEqual(eqArrays([[2, 3], [[4]]], [[2, 3], [[4]]]), false)); //false
