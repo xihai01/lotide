71d0edcc8ab514cb1daa48f1b27deddbf4debc91
Xihai Luo <xihai.luo01@gmail.com>
2021-11-01 12:47:01 +0000

    added line to export this function

--- assertEqual.js
+++ assertEqual.js
@@ -3,6 +3,9 @@ const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`🟩🟩🟩 Assertion Passed: ${actual} === ${expected}`) : console.log(`🟥🟥🟥 Assertion Failed: ${actual} !== ${expected}`);
 };
 
+//export the function
+module.exports = assertEqual;
+
 //test code
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
